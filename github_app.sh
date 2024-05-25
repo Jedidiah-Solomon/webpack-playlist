adb5fc1cf19a385daf4ddb74b481406def059a66
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-05-25 08:55:21 +0100

    Files update

--- github_app.sh
+++ github_app.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Prompt the user for a commit message
-echo "Enter your commit message:"
+echo "Enter your commit message: "
 read commit_msg
 
 # Commit with the provided message
@@ -16,7 +16,7 @@ current_branch=$(git branch --show-current)
 # Push to the current branch
 git push origin "$current_branch"
 
-echo "Action complted sucessfully"
+echo "Action completed sucessfully"
 
 #chmod +x github_app.sh  --to make it executable
 
